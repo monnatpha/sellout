439b49ed0ed8254157821ce815e8d2406ad1224d
Monnat Phanchaipol <monnat.pha@dplusonline.net>
2024-08-16 11:44:43 +0700

    11:44

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "db-saleplus-zt-do-user-16212651-0.c.db.ondigitalocean.com",
-  user: "saleplusdemo",
-  password: "AVNS_Cw3xBIZXgkL_Jjg05mI",
-  database: "TestMonnat",
-  port: 25060,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   connectTimeout: 50000,
 });
