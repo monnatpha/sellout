5da3d354c2abd2330e7cefb3c422f0d9b70ce563
Monnat Phanchaipol <monnat.pha@dplusonline.net>
2024-09-03 11:03:38 +0700

    11:03

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,11 @@ export default function Home(props) {
     <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
       <div className="container mx-auto p-4">
         {getMobileOperatingSystem() === "Android" ? (
-          <h1 className="text-3xl font-black mb-4 mt-72">
+          <h1 className="text-3xl font-black mb-4 mt-24">
             {`ติดฟิล์มโฟกัส ลุ้นของพรีเมียม 100 รางวัล ${"             "}`}
           </h1>
         ) : (
-          <h1 className="text-3xl font-black mb-4 mt-24">
+          <h1 className="text-3xl font-black mb-4 mt-4">
             ติดฟิล์มโฟกัส ลุ้นของพรีเมียม 100 รางวัล
           </h1>
         )}
